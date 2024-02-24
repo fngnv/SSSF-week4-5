50fcb01d1d32794e128f1802bff746b02a568d63
fngnv <finogenova.vera4@gmail.com>
2024-02-24 17:57:46 +0200

    build

--- test/testFunctions.ts
+++ test/testFunctions.ts
@@ -1,19 +1,19 @@
-/* eslint-disable node/no-unpublished-import */
-import {Application} from 'express';
-import request from 'supertest';
+// /* eslint-disable node/no-unpublished-import */
+// import {Application} from 'express';
+// import request from 'supertest';
 
-const getNotFound = (url: string | Application) => {
-  return new Promise((resolve, reject) => {
-    request(url)
-      .get('/what-is-this')
-      .expect(404, (err, response) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(response.body);
-        }
-      });
-  });
-};
+// const getNotFound = (url: string | Application) => {
+//   return new Promise((resolve, reject) => {
+//     request(url)
+//       .get('/what-is-this')
+//       .expect(404, (err, response) => {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           resolve(response.body);
+//         }
+//       });
+//   });
+// };
 
-export {getNotFound};
+// export {getNotFound};
